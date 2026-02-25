c81e4a3ae44d31e0e529cadf6df9b9989c15878f
nnamdi <uzonnamdi31@gmail.com>
2026-02-25 10:10:57 +0100

    modified ui

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,9 @@
 import ContactForm from "@/components/contact/ContactForm";
-import { ContactIntro } from "@/components/contact/ContactIntro";
 
 export default function Contact() {
     return (
         <div>
             <ContactForm />
-            {/* <ContactIntro /> */}
         </div>
     )
 }
