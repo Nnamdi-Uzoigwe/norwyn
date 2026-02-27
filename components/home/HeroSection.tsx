fc4a0ca0a687b6ba529e32926fa304ded3f756bd
nnamdi <uzonnamdi31@gmail.com>
2026-02-27 14:42:58 +0100

    changed hero content

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
       <Hero
         backgroundImage="/home-hero.jpg"
         title="Have You Been Affected by Financial Fraud?"
-        subtitle="We provide regulated forensic asset tracing services across both traditional and digital environments. Our specialists conduct detailed blockchain and financial analysis for cryptocurrency trading, legal action, freezing orders, and asset recovery, removing complexity and guiding you every step of the way"
+        subtitle="We provide regulated forensic asset tracing services across both traditional financial systems and digital environments. Our specialists deliver comprehensive blockchain and financial analysis to support cryptocurrency investigations, legal proceedings, asset freezing orders, and recovery efforts. We simplify complex financial and digital structures, guiding you with clarity and expertise at every stage of the process."
         ctaText="Request Immediate Case Review"
         ctaLink="/contact"
       />
